e6f54da4548e0bb33e07a41f6ecd65ef05fa2704
Mathieu Bodjikian (LAPTOP862078.local) <mathieu@bodjikian.fr>
2019-02-17 20:28:06 +0100

    Add provider basic handling

--- cmd/enedis2influx.go
+++ cmd/enedis2influx.go
@@ -16,6 +16,15 @@ var (
 )
 
 type EnedigoConfig struct {
+	Provider struct {
+		Name               string
+		AnnualFee          float64
+		MaxPower           int
+		PeakOffpeakEnabled bool
+		PricePerKwh        float64
+		PricePerKwhPeak    float64
+		PricePerKwhOffpeak float64
+	}
 	Enedis struct {
 		User           string                  `mapstructure:"user"`
 		Password       string                  `mapstructure:"password"`
@@ -34,7 +43,6 @@ type EnedigoConfig struct {
 func main() {
 
 	// Read config file
-	viper.SetDefault("days", 5)
 	viper.SetConfigName("enedigo")
 	viper.AddConfigPath(".")
 
@@ -75,7 +83,7 @@ func main() {
 	}
 
 	log.Printf("Getting data from enedis...")
-	measures, err := enedisClient.GetDataPerHour(time.Now().AddDate(0, 0, -5), time.Now())
+	measures, err := enedisClient.GetDataPerHour(time.Now().AddDate(0, 0, -*days), time.Now())
 	if err != nil {
 		log.Fatalf("Fail to get measures from enedis : %s", err)
 	}
@@ -87,21 +95,34 @@ func main() {
 	})
 
 	// Make some points
+	log.Printf("You are subscribed to %s", conf.Provider.Name)
+	log.Printf("	-> Price of your annual contract : %.2f €", conf.Provider.AnnualFee)
+	log.Printf("	-> Price of the kWH 			: %.4f €", conf.Provider.PricePerKwh)
+	log.Printf("	-> Price of the kWH (peak)   	 : %.4f €", conf.Provider.PricePerKwhPeak)
+	log.Printf("	-> Price of the kWH (off-peak)	 : %.4f €", conf.Provider.PricePerKwhOffpeak)
+
+	var totalPrice float64
 	for _, measure := range measures {
 
 		creuses := "0"
-		pleines := "1"
+		pleines := "0"
+		normales := "0"
 
-		if measure.IsOffpeak {
+		if len(conf.Enedis.OffpeakPeriods) == 0 {
+			normales = "1"
+		} else if measure.IsOffpeak {
 			creuses = "1"
 			pleines = "0"
+		} else {
+			creuses = "0"
+			pleines = "1"
 		}
 
 		// Create a point and add to batch
 		tags := map[string]string{
 			"heures_creuses":  creuses,
 			"heures_pleines":  pleines,
-			"heures_normales": "0",
+			"heures_normales": normales,
 		}
 		fields := map[string]interface{}{
 			"value": measure.Power * 1000,
@@ -115,8 +136,25 @@ func main() {
 
 		bp.AddPoint(pt)
 
-		log.Printf("Got measure of %s : %.3f | HC:%s | HP:%s", measure.Date.Format(time.RFC3339), measure.Power, creuses, pleines)
+		// Compute price
+		var priceToPay float64
+		if conf.Provider.PeakOffpeakEnabled {
+			if measure.IsOffpeak {
+				priceToPay = conf.Provider.PricePerKwhOffpeak * measure.Power
+			} else {
+				priceToPay = conf.Provider.PricePerKwhPeak * measure.Power
+			}
+		} else {
+			priceToPay = conf.Provider.PricePerKwh * measure.Power
+		}
+
+		totalPrice += priceToPay
+
+		log.Printf("Got measure of %s : %.3f | HC:%s | HP:%s | HN:%s | PRICE:%.4f", measure.Date.Format(time.RFC3339), measure.Power, creuses, pleines, normales, priceToPay)
+
 	}
+
+	log.Printf("Total price for period : %.4f", totalPrice)
 
 	// Write the batch
 	log.Printf("Pushing %d points to InfluxDB...", len(bp.Points()))
